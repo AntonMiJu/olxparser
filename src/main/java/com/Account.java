173e5bf1cfc1eb60508c55788fe94c0a440e37a4
anton <anton.mi.ju@gmail.com>
2020-01-26 17:30:03 +0200

    general commit

---
+++ src/main/java/com/Account.java
@@ -0,0 +1,40 @@
+package com;
+
+public class Account {
+    private String phone;
+    private String name;
+    private String address;
+    private String dateRegistered;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateRegistered() {
+        return dateRegistered;
+    }
+
+    public void setDateRegistered(String dateRegistered) {
+        this.dateRegistered = dateRegistered;
+    }
+}
---
+++ src/main/java/com/entities/AdStep.java
@@ -0,0 +1,4 @@
+package com.entities;
+
+public class AdStep {
+}
